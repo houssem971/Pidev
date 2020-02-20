fb42fef83cbfec92e3cc97b8a60c75217cf54fef
Houssem <houssem.ferjani@esprit.tn>
2020-02-20 16:50:12 +0100

    modifier blog

--- src/houssemBundle/Controller/blogController.php
+++ src/houssemBundle/Controller/blogController.php
@@ -67,6 +67,47 @@ class blogController extends Controller
         return $this->render('/backend/blog/consulterblog.html.twig', array('blog'=>$blog));
 
     }
+    /**
+     * @Route("admin/modifer/{id}",name="modif");
+     */
+    public function modifyAction(blog $blog,Request $request)
+    {
+
+        $form = $this->createForm(blogType::class, $blog);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            /**
+             * @var UploadedFile $file
+             */
+            $file1=$blog->getImage1();
+            $file2=$blog->getImage2();
+            $file3=$blog->getImage3();
+            $fileName1= md5(uniqid()).'.'.$file1->guessExtension();
+            $file1->move(
+                $this->getParameter('image_directory'),$fileName1
+            );
+            $fileName2= md5(uniqid()).'.'.$file2->guessExtension();
+            $file2->move(
+                $this->getParameter('image_directory'),$fileName2
+            );
+            $fileName3= md5(uniqid()).'.'.$file3->guessExtension();
+            $file3->move(
+                $this->getParameter('image_directory'),$fileName3
+            );
+            $blog->setImage1($fileName1);
+            $blog->setImage2($fileName2);
+            $blog->setImage3($fileName3);
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($blog);
+            $em->flush();
+            return $this->redirect($this->generateUrl('consulterblog'));
+        }
+
+        return $this->render('/backend/blog/ajoutblog.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
 
     /**
      * @Route("/admin/delate/{id}",name="supp");
